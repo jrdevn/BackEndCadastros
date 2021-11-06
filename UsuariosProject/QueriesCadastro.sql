455601513fa556290fcf2ac02588adddd2380e98
jhonatajr <48875248+jhonatajr@users.noreply.github.com>
2021-11-06 16:20:04 -0300

    Query

--- UsuariosProject/QueriesCadastro.sql
+++ UsuariosProject/QueriesCadastro.sql
@@ -1,1 +1,20 @@
-﻿
+﻿CREATE TABLE Usuario (
+	UsuarioId int  IDENTITY(1,1) NOT NULL,
+	Nome varchar(200) NOT NULL,
+	DataNascimento DATETIME NOT NULL,
+	Email varchar(100),
+	Senha varchar(30),
+	Ativo bit,
+	SexoId int NOT NULL,
+	PRIMARY KEY (UsuarioId),
+	FOREIGN KEY (SexoId) REFERENCES Sexo(SexoId)
+)
+
+CREATE TABLE Sexo (
+	SexoId int IDENTITY(1,1) NOT NULL,
+	Descricao varchar(15) NOT NULL,
+	PRIMARY KEY (SexoId)
+)
+
+insert into Sexo (Descricao) values ('Masculino');
+insert into Sexo (Descricao) values ('Feminino');
